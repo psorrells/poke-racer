fc249bca1e4b168a61b525935fdb07d1c58ccea9
psorrells <pamelan.sorrells@gmail.com>
2022-04-28 13:41:29 -0500

    add poke racers to the track

--- js/main.js
+++ js/main.js
@@ -1,38 +1,28 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const poke1 = document.querySelector('#poke1').value
-  const poke2 = document.querySelector('#poke2').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
-  const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
-  let pokeStore = []
-  let pokeImg = []
+let racers = []
 
+function getFetch(){
+  const poke = document.querySelector('#pokeAdd').value.toLowerCase()
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+poke
+  let pokeStore = {}
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        let pokeObj = {
+          name: data.name,
+          height: data.height,
+          weight: data.weight,
+          img: data.sprites.front_shiny
+        }
 
-        pokeStore.push(data.types[0].type.name)
-        pokeImg.push(data.sprites.front_shiny)
-        
-        fetch(url2)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
+        racers.push(pokeObj)
 
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.front_shiny)
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-            document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
-            document.querySelector('h2').innerText = " 2x > "
-          }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-
+        let curPoke = document.createElement('img')
+        curPoke.src = pokeObj.img
+        curPoke.id = pokeObj.name
+        document.querySelector('.racetrack').appendChild(curPoke)
 
       })
       .catch(err => {
